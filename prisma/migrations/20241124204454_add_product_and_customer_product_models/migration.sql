41af7c30d0958779fd4fa7caa1368d8f52ec4b4d
Noam Sauer-Utley <noamsauerutley@gmail.com>
2024-11-24 15:45:26 -0500

    Add product tables

---
+++ prisma/migrations/20241124204454_add_product_and_customer_product_models/migration.sql
@@ -0,0 +1,50 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `Customer` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateEnum
+CREATE TYPE "unitOfMeasure" AS ENUM ('bag', 'barrel', 'box', 'bunch', 'bundle', 'butt', 'canister', 'carton', 'case', 'container', 'crate', 'cubicCentimeter', 'cubicFoot', 'cubicInch', 'cubicMeter', 'cubicYard', 'cup', 'dozen', 'each', 'gallon', 'gram', 'gross', 'kilogram', 'liter', 'metricTon', 'milliliter', 'ounce', 'pair', 'piece', 'pint', 'pound', 'quart', 'set', 'ton', 'tray');
+
+-- AlterTable
+ALTER TABLE "Customer" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "customerId" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "unitOfMeasure" "unitOfMeasure" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CustomerProduct" (
+    "id" SERIAL NOT NULL,
+    "customerId" INTEGER NOT NULL,
+    "productId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "CustomerProduct_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_customerId_name_key" ON "Product"("customerId", "name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "CustomerProduct_customerId_productId_key" ON "CustomerProduct"("customerId", "productId");
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "Customer"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CustomerProduct" ADD CONSTRAINT "CustomerProduct_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "Customer"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CustomerProduct" ADD CONSTRAINT "CustomerProduct_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
